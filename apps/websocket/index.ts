c5350041e87deb7df7374ff9f00048e1ddc7fd2b
netizen-om <borisagarom@gmail.com>
2025-12-25 11:36:38 +0530

    added backend and websocket server and their basic routes

---
+++ apps/websocket/index.ts
@@ -0,0 +1,36 @@
+import { prismaClient } from "db/client";
+
+Bun.serve({
+  port: 8081,
+  fetch(req, server) {
+    // upgrade the request to a WebSocket
+    if (server.upgrade(req)) {
+      return; // do not return a Response
+    }
+    console.log("Websocket App listening on port 8080");
+    return new Response("Upgrade failed", { status: 500 });
+  },
+  websocket: {
+    message(ws, message) {
+      console.log("Creating USer");
+
+      const createUser = async () => {
+        try {
+          const user = await prismaClient.user.create({
+            data: {
+              username: Math.random().toString(),
+              password: Math.random().toString(),
+            },
+          });
+          console.log(user);
+        } catch (error) {
+          console.log("Error while creating User \n", error);
+        }
+      };
+
+      createUser();
+
+      ws.send(message);
+    },
+  },
+});
